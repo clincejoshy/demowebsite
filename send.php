7169e3a249ce0955bb4277ae2314034ac2641b07
clincejoshy <clincepro@gmail.com>
2016-11-17 19:57:33 +0530

    xs
    ss

--- send.php
+++ send.php
@@ -1,14 +1,9 @@
 <?php
 $to=$_POST['to'];
 $subject=$_POST['subject'];
-$name=$_POST['name'];
 $from=$_POST['from'];
-$type='t';
-$content=$_POST['content'];
-mail($to,$subject,$content,”From:$name<$from>\r\nContent-Type:$type”);
-echo(“<p style=”color:green”>E-Mail Sent!</p>”);
-}
-else{
-         echo(“<p style=”color:red”>E-Mail NOT Sent!</p>”);
-}
+$txt=$_POST['content'];
+$headers = "From:".$txt;
+
+mail($to,$subject,$txt,$headers);
 ?>
