c919135857de4cc7ee39e565bf59a8863dc156a8
clincejoshy <clincepro@gmail.com>
2016-11-17 20:03:24 +0530

    s
    s

--- send.php
+++ send.php
@@ -3,7 +3,7 @@ $to=$_POST['to'];
 $subject=$_POST['subject'];
 $from=$_POST['from'];
 $txt=$_POST['content'];
-$headers = "From:".$txt;
+$headers = "From: sreerag939@gmail.com";
 
 mail($to,$subject,$txt,$headers);
 ?>
