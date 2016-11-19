05e27c275bffdb8ac9ce6b64d12232e88cb48fb0
clincejoshy <clincepro@gmail.com>
2016-11-19 14:45:16 +0530

    login page
    login

--- send.php
+++
@@ -1,9 +0,0 @@
-<?php
-$to=$_POST['to'];
-$subject=$_POST['subject'];
-$from=$_POST['from'];
-$txt=$_POST['content'];
-$headers = "From: xyz@anaon.com" . "\r\n";
-
-mail($to,$subject,$txt,$headers);
-?>
